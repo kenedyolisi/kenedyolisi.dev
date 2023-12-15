687dd238c10adff5f89d9f2f1679ec09797c3c8a
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2023-12-15 15:18:16 +0300

    chore: add react integration

--- astro.config.ts
+++ astro.config.ts
@@ -1,15 +1,17 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel({
     webAnalytics: {
-      enabled: true,
+      enabled: true
     },
     speedInsights: {
-      enabled: true,
-    },
+      enabled: true
+    }
   }),
+  integrations: [react()]
 });

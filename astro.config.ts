6fb5a7f8fbdd57ae308ab5799cc3e244ce695bff
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2023-12-16 14:26:32 +0300

    chore: update astro configuration
    Remove integrations.

--- astro.config.ts
+++ astro.config.ts
@@ -1,17 +1,4 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
-import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({
-  output: "server",
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true
-    },
-    speedInsights: {
-      enabled: true
-    }
-  }),
-  integrations: [react()]
-});
+export default defineConfig({});

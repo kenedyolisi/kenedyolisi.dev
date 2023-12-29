3a5f5a404c40f65052e0ea4ada367aea3f0ba60b
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2023-12-29 13:00:35 +0300

    chore: update astro configuration

--- astro.config.ts
+++ astro.config.ts
@@ -1,14 +1,7 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
   integrations: [react()],
 });

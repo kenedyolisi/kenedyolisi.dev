c117a07fa72da096e8a3c37ea8d855b864b690e5
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2023-12-29 22:09:33 +0300

    refactor: add comment

--- astro.config.ts
+++ astro.config.ts
@@ -3,5 +3,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [
+    react(), // Add react integration for react components
+  ],
 });

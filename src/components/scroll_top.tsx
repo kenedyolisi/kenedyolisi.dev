125a6d6310f7da6f6af3a38b6cc44a5992432aba
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2023-12-29 22:35:21 +0300

    refactor(components): export component as default

--- src/components/scroll_top.tsx
+++ src/components/scroll_top.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 
-export const ScrollTopBtn = () => {
+export default function ScrollTopBtn() {
   const [show, setShow] = useState(false);
   const prevScrollY = useRef(0);
 
@@ -34,29 +34,27 @@ export const ScrollTopBtn = () => {
   }, []); // Empty dependency array ensures the effect runs once after the initial render
 
   return (
-    <>
-      <button
-        className={`btn btn-primary scroll-to-top rounded-circle z-3 position-fixed ${
-          show ? "showing" : ""
-        }`}
-        id="scroll-to-top"
-        type="button"
-        title="scroll to top"
-        aria-label="scroll to top"
-        onClick={handleClick}
+    <button
+      className={`btn btn-primary scroll-to-top rounded-circle z-3 position-fixed ${
+        show ? "showing" : ""
+      }`}
+      id="scroll-to-top"
+      type="button"
+      title="scroll to top"
+      aria-label="scroll to top"
+      onClick={handleClick}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 16 16"
+        width="16"
+        fill="currentColor"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 16 16"
-          width="16"
-          fill="currentColor"
-        >
-          <path
-            fillRule="evenodd"
-            d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5"
-          ></path>
-        </svg>
-      </button>
-    </>
+        <path
+          fillRule="evenodd"
+          d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5"
+        ></path>
+      </svg>
+    </button>
   );
-};
+}

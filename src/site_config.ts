9562a02708268fc2c1918ffa93706a1e53c16b80
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2023-12-29 23:45:32 +0300

    refactor: rename title to owner

--- src/site_config.ts
+++ src/site_config.ts
@@ -2,7 +2,7 @@ export const siteName = "kenedyolisi.vercel.app";
 
 const siteConfig = {
   siteUrl: `https://${siteName}`,
-  title: "Kenedy Olisi",
+  owner: "Kenedy Olisi",
   slogan: "Front-end Developer",
   description:
     "Front-End Developer based in Kenya creating visually stunning websites for businesses and individuals. I specialize in mobile-first and responsive design, UI/UX, and web optimization. Let's collaborate! 🚀",

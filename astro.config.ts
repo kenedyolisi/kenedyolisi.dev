f4a5c360610546ca3f91355ccb0c337af1b25f42
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-01-01 22:18:51 +0300

    chore: update astro configuration

--- astro.config.ts
+++ astro.config.ts
@@ -4,7 +4,9 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: siteConfig.siteUrl,
-  output: "server",
+  output: "hybrid",
   adapter: vercel({ imageService: true }),
+  build: { format: "file" },
+  site: siteConfig.siteUrl,
+  trailingSlash: "never",
 });

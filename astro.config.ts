fafa1bb82ac18d506be86ce1d706fc47d8da7206
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-01-02 00:46:43 +0300

    chore: add icon integration in astro configuration

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,30 @@
 import { defineConfig } from "astro/config";
 import { siteConfig } from "./src/site_config";
+import icon from "astro-icon";
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  adapter: vercel({ imageService: true }),
-  build: { format: "file" },
+  adapter: vercel({
+    imageService: true,
+  }),
+  integrations: [
+    icon({
+      include: {
+        bi: [
+          "box-arrow-up-right",
+          "facebook",
+          "github",
+          "linkedin",
+          "twitter-x",
+        ],
+      },
+    }),
+  ],
+  build: {
+    format: "file",
+  },
   site: siteConfig.siteUrl,
   trailingSlash: "never",
 });

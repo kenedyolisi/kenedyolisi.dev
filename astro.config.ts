46e0bb14f6e6ff02d08b239fe9b0b1d3075ac7df
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-01-09 17:31:11 +0300

    chore: add tailwind integration

--- astro.config.ts
+++ astro.config.ts
@@ -3,24 +3,18 @@ import { siteConfig } from "./src/site_config";
 import icon from "astro-icon";
 import vercel from "@astrojs/vercel/serverless";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
   adapter: vercel({
-    imageService: true,
+    imageService: true
   }),
-  integrations: [
-    icon({
-      include: {
-        bi: [
-          "box-arrow-up-right",
-          "facebook",
-          "github",
-          "linkedin",
-          "twitter-x",
-        ],
-      },
-    }),
-  ],
-  site: siteConfig.siteUrl,
+  integrations: [icon({
+    include: {
+      bi: ["box-arrow-up-right", "facebook", "github", "linkedin", "twitter-x"]
+    }
+  }), tailwind()],
+  site: siteConfig.siteUrl
 });

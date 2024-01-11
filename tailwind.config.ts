243bde6b9f733661bf5b9acb62980a7d16bf9ed2
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-01-11 11:44:12 +0300

    chore: update tailwindcss configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss";
 import plugin from "tailwindcss/plugin";
 
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}"],
   theme: {
     extend: {},
   },

c580d8910259e4194f09f3653e4d6cec20cbee84
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-01-12 15:41:00 +0300

    chore: update tailwindcss configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,29 @@ import plugin from "tailwindcss/plugin";
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: "425px",
+      },
+      colors: {
+        primary: {
+          DEFAULT: "#106cfe",
+          dark: "#468cfc",
+        },
+
+        success: {
+          DEFAULT: "#0f8750",
+          dark: "#14a35a",
+        },
+
+        danger: {
+          DEFAULT: "#dc324b",
+          dark: "#ff4b5a",
+        },
+
+        dark: "#1e2832",
+      },
+    },
   },
   plugins: [
     plugin(function ({ addComponents, addUtilities, theme }) {
@@ -12,6 +34,9 @@ export default {
         // ".card": {
         //   padding: theme("padding.2"),
         // },
+        ".col": {
+          flex: "1 0 0%",
+        },
       });
 
       addUtilities({});

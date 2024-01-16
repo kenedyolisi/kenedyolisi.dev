ae44e252342c5906aa3b9318275e7accb2341e5e
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-01-16 22:12:14 +0300

    feat(css): update tailwindcss configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from "tailwindcss";
-import plugin from "tailwindcss/plugin";
 
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}"],
@@ -28,18 +27,4 @@ export default {
       },
     },
   },
-  plugins: [
-    plugin(function ({ addComponents, addUtilities, theme }) {
-      addComponents({
-        // ".card": {
-        //   padding: theme("padding.2"),
-        // },
-        ".col": {
-          flex: "1 0 0%",
-        },
-      });
-
-      addUtilities({});
-    }),
-  ],
 } satisfies Config;

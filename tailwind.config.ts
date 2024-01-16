36949f57acb50aa10842ce2e64b496122a9c06ec
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-01-17 00:45:43 +0300

    feat(css): update tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,19 @@
 import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+
       screens: {
         xs: "425px",
       },
+
       colors: {
         primary: {
           DEFAULT: "#106cfe",
@@ -24,6 +31,23 @@ export default {
         },
 
         dark: "#1e2832",
+      },
+
+      animation: {
+        "zoom-out": "zoom-out 300ms ease-out",
+
+        "zoom-in": "zoom-in 300ms ease-in",
+      },
+
+      keyframes: {
+        "zoom-out": {
+          from: { transform: "scale(1)", visibility: "visible" },
+          to: { transform: "scale(0)", visibility: "hidden" },
+        },
+        "zoom-in": {
+          from: { transform: "scale(0)", visibility: "hidden" },
+          to: { trnasform: "scale(1)", visibility: "visible" },
+        },
       },
     },
   },

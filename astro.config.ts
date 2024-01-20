786d2fc42ae04b3de9faf03ab5770a45d0450fa0
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-01-20 23:41:06 +0300

    chore: remove react integration

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import { siteConfig } from "./src/site_config";
 import icon from "astro-icon";
-import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
@@ -16,7 +15,6 @@ export default defineConfig({
       applyBaseStyles: false,
     }),
     icon({}),
-    react(),
   ],
   site: siteConfig.siteUrl,
 });

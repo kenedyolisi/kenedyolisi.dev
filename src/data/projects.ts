6c36e4cfc200445b03c3ea63f1e8261b6d82c66e
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-02-23 22:33:43 +0300

    refactor(data): update projects data

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,27 +1,31 @@
+import technologies from "@data/technologies.json";
+
 interface Project {
   title: string;
-  technologies: string[];
+  techs: (keyof typeof technologies)[];
   url: string;
   image: string;
 }
 
-export const projects: Project[] = [
+const projects: Project[] = [
   {
     title: "kenedyolisi landing page",
-    technologies: ["astro", "tailwind"],
+    techs: ["astro", "tailwindcss"],
     url: "https://kenedyolisi.vercel.app",
     image: "src/images/kenedyolisi_landing_page.png",
   },
   {
     title: "nextjs dashboard",
-    technologies: ["nextjs", "react"],
+    techs: ["nextjs", "react"],
     image: "src/images/nextjs_dashboard.png",
     url: "https://kenedyolisi-nextjs-dashboard.vercel.app",
   },
   {
     image: "src/images/random_quote_machine.png",
-    technologies: ["react", "tailwindcss"],
+    techs: ["react", "tailwindcss"],
     title: "random quote machine",
     url: "https://kenedyolisi.gihub.io/random-quote-machine",
   },
 ];
+
+export default projects;

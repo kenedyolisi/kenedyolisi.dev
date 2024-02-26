b5b0620d19a9d70725c987d8f4a511e78caaa7ad
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-02-26 08:28:43 +0300

    chore: update astro config
    Add react integration.

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,7 @@
 import partytown from "@astrojs/partytown";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
+import react from "@astrojs/react";
 import { defineConfig } from "astro/config";
 import { siteConfig } from "./src/site_config";
 
@@ -14,6 +15,7 @@ export default defineConfig({
     tailwind({
       applyBaseStyles: false,
     }),
+    react(),
     partytown({ config: { debug: false, forward: ["datalayer.push"] } }),
   ],
   site: siteConfig.siteUrl,

6f99b6b6fc982a7d2a1718011e0e38e50596d92d
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-02-26 22:47:06 +0300

    feat: update utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,8 @@
 export const isBrowser = typeof window !== "undefined";
+
+export function capitalizeStr(str: string) {
+  return str
+    .split(" ")
+    .map((subStr) => subStr.slice(0, 1).toUpperCase() + subStr.slice(1))
+    .join(" ");
+}

c28414380916e542ccbc5da0debff4a384ad260d
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-03-14 00:39:35 +0300

    feat(components): create project

---
+++ src/components/project.tsx
@@ -0,0 +1,16 @@
+import type { Project as IProject } from "@data/projects";
+
+interface Props {
+  project: IProject;
+}
+
+export default function Project({ project }: Props) {
+  const { image, title } = project;
+
+  return (
+    <div className="border rounded-lg">
+      <img className="rounded-t-lg aspect-video" src={image} alt={title} loading="lazy" decoding="async" />
+      <h2>{title }</h2>
+    </div>
+  );
+}

a0a5a8b1142c5489c39b498e90e87ab497364b67
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-03-14 21:54:56 +0300

    feat(components): create projects list

---
+++ src/components/projects_list.tsx
@@ -0,0 +1,15 @@
+import Project from "@components/project";
+import { type Project as IProject } from "@data/projects";
+
+interface Props {
+  projects: IProject[];
+}
+export default function ProjectsList({ projects }: Props) {
+  return (
+    <ul className="grid gap-5 grid-cols-1 md:grid-cols-2">
+      {projects.map((project) => (
+        <Project project={project} key={project.id} />
+      ))}
+    </ul>
+  );
+}

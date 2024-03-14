e03c5cefa2e53c6e0dca76520d8814cc4ccadaab
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-03-14 22:55:42 +0300

    chore: update types

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,2 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-
-export type IconKey = keyof typeof import("@data/icons.json");
-export type TechKey = keyof typeof import("@data/technologies.json");
-export type Theme = "dark" | "light" | "system";

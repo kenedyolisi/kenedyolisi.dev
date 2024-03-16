ecfa1d4666f3b0d29cbe43c57e1c849ccbedc9f5
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-03-16 23:56:46 +0300

    refactor(components): replace react with svelte

---
+++ src/components/projects_list.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import Project from "@components/project.svelte";
+  import { type Project as IProject } from "@data/projects";
+
+  export let projects: IProject[];
+</script>
+
+<ul class="grid gap-5 grid-cols-1 md:grid-cols-2">
+  {#each projects as project}
+    <Project {project} />
+  {/each}
+</ul>

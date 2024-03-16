9bea26c71b0e851f356c1eb0e122703f92fdfa8a
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-03-17 01:00:57 +0300

    fix(components): add list key

--- src/components/projects_list.svelte
+++ src/components/projects_list.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <ul class="grid gap-5 grid-cols-1 md:grid-cols-2">
-  {#each projects as project}
+  {#each projects as project (project.id)}
     <Project {project} />
   {/each}
 </ul>

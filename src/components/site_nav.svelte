6fc9b41ba31ce535d1deb15ae9c27db7cb104357
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-03-17 22:41:09 +0300

    refactor(components): replace astro component with svelte

---
+++ src/components/site_nav.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  const navLinks = [
+    "about",
+    "services",
+    "showcase",
+    // "testimonials",
+    "blog",
+    "contact",
+  ];
+</script>
+
+<nav>
+  <ul class="flex flex-col md:flex-row gap-1">
+    {#each navLinks as navLink, index (index)}
+      <li>
+        <a
+          class={`block md:inline-block p-2 capitalize no-underline
+            md:after:content-[''] md:after:h-2 md:after:bg-primary`}
+          href="/{navLink === 'home' ? '' : navLink}"
+        >
+          {navLink}
+        </a>
+      </li>
+    {/each}
+  </ul>
+</nav>

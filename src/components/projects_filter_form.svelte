b4d7788b38fa16c51d76572bdb24b902cd4df19c
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-03-18 10:04:19 +0300

    refactor(components): replace react with svelte

---
+++ src/components/projects_filter_form.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+  import projects from "@data/projects";
+
+  const technologies = [...new Set(projects.map(({ techs }) => techs).flat())];
+  const categories = [
+    ...new Set(projects.map(({ categories }) => categories).flat()),
+  ];
+</script>
+
+<form class="space-y-3">
+  <details class="space-y-2" open>
+    <summary class="flex justify-between items-center cursor-pointer">
+      <strong>Technology</strong>
+    </summary>
+
+    <div class="flex flex-col space-y-1">
+      {#each technologies as technology (technology)}
+        <label
+          class="inline-flex items-center gap-2 py-2 px-4 capitalize hover:bg-gray-300 dark:hover:bg-gray-600 rounded-lg cursor-pointer"
+        >
+          <input name="technologies" value={technology} type="checkbox" />
+          {technology}
+        </label>
+      {/each}
+    </div>
+  </details>
+
+  <details class="space-y-3" open>
+    <summary class="flex justify-between items-center cursor-pointer">
+      <strong>Categories</strong>
+    </summary>
+    <div class="flex flex-col space-y-1">
+      {#each categories as category (category)}
+        <label
+          class="inline-flex items-center gap-2 py-2 px-4 capitalize hover:bg-gray-300 dark:hover:bg-gray-600 rounded-lg cursor-pointer"
+        >
+          <input name="categories" value={category} type="checkbox" />
+          {category}
+        </label>
+      {/each}
+    </div>
+  </details>
+
+  <button
+    class="p-2 border rounded-xl bg-primary dark:bg-primary-dark text-white"
+    type="submit"
+  >
+    View Results
+  </button>
+</form>
+
+<style>
+  details[open] summary svg {
+    @apply rotate-90;
+  }
+</style>

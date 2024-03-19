3d4d1b12cf54060a645215cf7afe3513454de346
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-03-19 21:45:13 +0300

    feat(icons): add more icons

--- src/components/page_header.svelte
+++ src/components/page_header.svelte
@@ -1,6 +1,5 @@
-<script>
+<script lang="ts">
   import Logo from "@components/logo.svelte";
-  import Icon from "@iconify/svelte";
   import SiteNav from "@components/site_nav.svelte";
   import ThemeMenu from "@components/theme_menu.svelte";
   import { clickOutside } from "src/utils";
@@ -35,7 +34,7 @@
         class="h-fit p-3 border ronded-md"
         on:click={() => (isOpen = false)}
       >
-        <Icon icon="bi:x-lg" width={16} class="text-dark" />
+        <span class="icon icon-x"></span>
       </button>
     </div>
     <SiteNav />
@@ -48,19 +47,6 @@
     type="button"
     on:click={() => (isOpen = !isOpen)}
   >
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 16 16"
-      fill="currentColor"
-      width={25}
-    >
-      <path
-        d="M0 12.344h1.46l.564 2.187.561-2.187h1.456v3.592h-.906v-2.739l-.704 2.739h-.82l-.702-2.739v2.739H0zm4.747 0H7.72v.767H5.86v.572h1.727v.733H5.86v.707h1.916v.813h-3.03zm3.634 0h1.036l1.354 1.987v-1.987h1.046v3.592h-1.046l-1.347-1.973v1.973H8.381zm6.511 0H16v2.141q0 .32-.1.604-.099.28-.312.494-.21.212-.443.297-.324.12-.777.12-.263 0-.573-.036-.31-.037-.518-.145-.208-.11-.382-.311-.171-.2-.235-.413-.103-.344-.103-.61v-2.141h1.107v2.193q0 .294.163.462.164.164.453.164.287 0 .448-.163.164-.164.164-.463z"
-      ></path>
-      <path
-        d="M1.333 0h13.334A1.33 1.33 0 0116 1.333a1.33 1.33 0 01-1.333 1.334H1.333A1.33 1.33 0 010 1.333 1.33 1.33 0 011.333 0zm0 6.172h13.334A1.33 1.33 0 0116 7.505a1.33 1.33 0 01-1.333 1.334H1.333A1.33 1.33 0 010 7.505a1.33 1.33 0 011.333-1.333z"
-        fill-rule="evenodd"
-      ></path>
-    </svg>
+    <span class="icon icon-menu"></span>
   </button>
 </header>

e975ee1f9033a500f49aca513539b8e97a7d50ac
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-03-22 07:44:20 +0300

    refactor(components): update project filter form

--- src/components/projects_filter_form.svelte
+++ src/components/projects_filter_form.svelte
@@ -8,38 +8,46 @@
 </script>
 
 <form class="space-y-3">
-  <details class="space-y-2" open>
-    <summary class="flex justify-between items-center cursor-pointer">
-      <strong>Technology</strong>
-    </summary>
+  {#if technologies.length}
+    <details class="space-y-2" open>
+      <summary class="flex justify-between items-center cursor-pointer">
+        <strong>Technology</strong>
+        <span class="icon icon-chevron-right transition-transform duration-200"
+        ></span>
+      </summary>
 
-    <div class="flex flex-col space-y-1">
-      {#each technologies as technology (technology)}
-        <label
-          class="inline-flex items-center gap-2 py-2 px-4 capitalize hover:bg-gray-300 dark:hover:bg-gray-600 rounded-lg cursor-pointer"
-        >
-          <input name="technologies" value={technology} type="checkbox" />
-          {technology}
-        </label>
-      {/each}
-    </div>
-  </details>
+      <div class="flex flex-col space-y-1">
+        {#each technologies as technology (technology)}
+          <label
+            class="inline-flex items-center gap-2 py-2 px-4 capitalize hover:bg-gray-300 dark:hover:bg-gray-600 rounded-lg cursor-pointer"
+          >
+            <input name="technologies" value={technology} type="checkbox" />
+            {technology}
+          </label>
+        {/each}
+      </div>
+    </details>
+  {/if}
 
-  <details class="space-y-3" open>
-    <summary class="flex justify-between items-center cursor-pointer">
-      <strong>Categories</strong>
-    </summary>
-    <div class="flex flex-col space-y-1">
-      {#each categories as category (category)}
-        <label
-          class="inline-flex items-center gap-2 py-2 px-4 capitalize hover:bg-gray-300 dark:hover:bg-gray-600 rounded-lg cursor-pointer"
-        >
-          <input name="categories" value={category} type="checkbox" />
-          {category}
-        </label>
-      {/each}
-    </div>
-  </details>
+  {#if categories.length}
+    <details class="space-y-3" open>
+      <summary class="flex justify-between items-center cursor-pointer">
+        <strong>Categories</strong>
+        <span class="icon icon-chevron-right transition-transform duration-200"
+        ></span>
+      </summary>
+      <div class="flex flex-col space-y-1">
+        {#each categories as category (category)}
+          <label
+            class="inline-flex items-center gap-2 py-2 px-4 capitalize hover:bg-gray-300 dark:hover:bg-gray-600 rounded-lg cursor-pointer"
+          >
+            <input name="categories" value={category} type="checkbox" />
+            {category}
+          </label>
+        {/each}
+      </div>
+    </details>
+  {/if}
 
   <button
     class="p-2 border rounded-xl bg-primary dark:bg-primary-dark text-white"
@@ -50,7 +58,7 @@
 </form>
 
 <style>
-  details[open] summary svg {
+  details[open] .icon {
     @apply rotate-90;
   }
 </style>

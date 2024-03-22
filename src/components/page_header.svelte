3e03a1ccfe8496cbc4b45a46bf95ca7676083a2d
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-03-22 07:02:15 +0300

    feat(components): update page header

--- src/components/page_header.svelte
+++ src/components/page_header.svelte
@@ -2,19 +2,34 @@
   import Logo from "@components/logo.svelte";
   import SiteNav from "@components/site_nav.svelte";
   import ThemeMenu from "@components/theme_menu.svelte";
-  import { clickOutside } from "src/utils";
+  import { clickOut } from "src/utils";
 
   let isOpen = false;
+  let showShadow = false;
+
+  function handleScroll() {
+    if (scrollY > 100) {
+      showShadow = true;
+    } else {
+      showShadow = false;
+    }
+  }
 </script>
 
+<svelte:window on:scroll={handleScroll} />
+
 <header
-  class="sticky top-0 z-20 flex justify-between items-center gap-3 w-full py-3 px-5 bg-white dark:bg-dark"
+  class={`
+  sticky top-0 z-10 flex justify-between items-center gap-3 w-full 
+  py-2 px-5 bg-white dark:bg-dark
+  ${showShadow ? "shadow-md" : ""}
+  `}
 >
   <a href="/"><Logo /></a>
 
   <div
     class={`
-    fixed md:static top-0 right-0 bottom-0
+    fixed md:static top-0 md:top-auto right-0 md:right-auto bottom-0 md:bottom-auto z-20
     flex flex-col md:flex-row md:flex-grow md:justify-between md:items-center gap-4
     w-full xs:w-3/5 md:w-auto
     py-3 px-5 md:p-0
@@ -22,28 +37,26 @@
     bg-white dark:bg-dark md:bg-transparent
     md:translate-x-0
     transition-transform
-    duration-300 focus:not:translate-x-full
-    ${isOpen ? "translate-x-0" : "translate-x-full"}
+    duration-300
+    ${!isOpen ? "translate-x-full" : ""}
     `}
-    use:clickOutside
-    on:outclick={() => (isOpen = false)}
+    use:clickOut
+    on:clickout={() => (isOpen = false)}
   >
     <div class="flex justify-between items-center md:hidden">
       <a href="/"><Logo /></a>
       <button
-        class="h-fit p-3 border ronded-md"
+        class="p-2 leading-4 border rounded-md"
         on:click={() => (isOpen = false)}
       >
-        <span class="icon icon-x"></span>
+        <span class="w-6 h-6 icon icon-x"></span>
       </button>
     </div>
     <SiteNav />
-    <hr class="md:hidden" />
-    <ThemeMenu />
   </div>
-
+  <ThemeMenu />
   <button
-    class="h-fit p-2 md:hidden border rounded-md"
+    class="p-2 leading-4 md:hidden border rounded-md"
     type="button"
     on:click={() => (isOpen = !isOpen)}
   >

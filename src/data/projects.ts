172e4e5105a6b8c790595b5a9c15a472b0c8413d
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-03-22 07:47:12 +0300

    feat(data): update projects data

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,10 +1,8 @@
-import type { TechKey } from "src/env";
-
 export interface Project {
   id: string;
   title: string;
   categories: string[];
-  techs: TechKey[];
+  techs: string[];
   url: string;
   image: string;
 }
@@ -14,16 +12,16 @@ export default [
     id: "d652a935-3caf-5015-9e62-628dab47c87c",
     title: "my portfolio",
     categories: ["portfolio"],
-    techs: ["astro", "tailwindcss", "react"],
+    techs: ["astro", "tailwindcss", "svelte"],
     url: "https://kenedyolisi.vercel.app",
     image: "/src/images/kenedyolisi_landing_page.png",
   },
   {
     id: "5f2bf98c-3af2-574c-a7aa-3882e7144fd2",
     image: "/src/images/random_quote_machine.png",
-    categories: ["SPA"],
+    categories: [],
     techs: ["react", "tailwindcss"],
-    title: "random quote machine",
-    url: "https://kenedyolisi.gihub.io/random-quote-machine",
+    title: "random quote generator",
+    url: "https://kenedyolisi.gihub.io/random-quote-generator",
   },
 ] satisfies Project[];

adbb2ceddc51244dfeec4b060d027cf7a3f1baf9
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-05-07 02:10:49 +0300

    feat(data): update projects data

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,13 +1,15 @@
 export interface Project {
+  id: string;
   title: string;
   categories: string[];
   techs: string[];
-  url?: string;
+  url: string;
   image: string;
 }
 
 export default [
   {
+    id: "d652a935-3caf-5015-9e62-628dab47c87c",
     title: "my portfolio",
     categories: ["portfolio"],
     techs: ["astro", "tailwindcss", "svelte"],
@@ -15,9 +17,10 @@ export default [
     image: "/src/images/kenedyolisi_landing_page.png",
   },
   {
+    id: "5f2bf98c-3af2-574c-a7aa-3882e7144fd2",
     image: "/src/images/random_quote_generator.png",
     categories: [],
-    techs: ["svelte", "tailwindcss"],
+    techs: ["react", "tailwindcss"],
     title: "random quote generator",
     url: "https://kenedyolisi.gihub.io/random-quote-generator",
   },

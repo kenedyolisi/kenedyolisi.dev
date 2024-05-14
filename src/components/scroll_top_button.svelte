04392b186e4cbd7dd16d6dd039208f5e335d7d35
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-05-14 20:38:31 +0300

    refactor(components): rename variables

--- src/components/scroll_top_button.svelte
+++ src/components/scroll_top_button.svelte
@@ -1,22 +1,22 @@
 <script>
   import { scale } from "svelte/transition";
 
-  let prevY = $state(0);
-  let show = $state(false);
+  let previousScrollY = $state(0);
+  let isVisible = $state(false);
 
   function handleScroll() {
-    if (scrollY < 100 || scrollY > prevY) {
-      show = false;
+    if (scrollY < 100 || scrollY > previousScrollY) {
+      isVisible = false;
     } else {
-      show = true;
+      isVisible = true;
     }
-    prevY = scrollY;
+    previousScrollY = scrollY;
   }
 </script>
 
 <svelte:window on:scroll={handleScroll} />
 
-{#if show}
+{#if isVisible}
   <button
     class="fixed right-6 bottom-10 p-3 leading-4 rounded-full bg-primary dark:bg-primary-dark text-white"
     title="scroll to top"

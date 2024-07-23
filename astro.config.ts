2d1b7c71945459792b9db3f71a757710cfebb71e
Kenedy Olisi <kenedyolisi@gmail.com>
2024-07-23 21:03:39 +0300

    feat(integration): add webvitals

--- astro.config.ts
+++ astro.config.ts
@@ -2,6 +2,7 @@ import partytown from "@astrojs/partytown";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
+import webVitals from "@astrojs/web-vitals";
 import { defineConfig } from "astro/config";
 import { siteConfig } from "./src/site_config";
 
@@ -12,17 +13,18 @@ export default defineConfig({
     imageService: true,
   }),
   integrations: [
-    svelte(),
     tailwind({
       applyBaseStyles: false,
       nesting: true,
     }),
+    svelte(),
     partytown({
       config: {
         debug: false,
         forward: ["datalayer.push"],
       },
     }),
+    webVitals(),
   ],
   site: siteConfig.siteUrl,
 });
